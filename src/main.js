d6cb3cb52e1ac00b9e2d8071cf8c67f0a83de233
Aleix <aleixriba33@gmail.com>
2020-01-07 23:12:32 +0100

    install and configure firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const firebase = require('firebase/app');
+
+require('firebase/auth');
+// require('firebase/database');
+// require('firebase/firestore');
+// require('firebase/messaging');
+// require('firebase/functions');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCa0ZgmDMfoj_JQpfDqSFrXjdEVCoUuSgA",
+  authDomain: "vue-crud-92877.firebaseapp.com",
+  databaseURL: "https://vue-crud-92877.firebaseio.com",
+  projectId: "vue-crud-92877",
+  storageBucket: "vue-crud-92877.appspot.com",
+  messagingSenderId: "987791791083",
+  appId: "1:987791791083:web:762cc322d9e5d89e833fae"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
