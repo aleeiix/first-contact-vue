925d069d2affe857988f0214d1a4a9f82c671a9b
Aleix <aleixriba33@gmail.com>
2020-01-09 00:09:53 +0100

    register finish

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+var firebase = require("firebase/app")
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: null,
+    error: null
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    }
   },
   actions: {
+    registerUser({commit}, payload) {
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+          .then((res) => {
+            commit('setUser', {uid: res.user.uid, email: res.user.email});
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+    }
   },
   modules: {
   }
---
+++ src/views/Register.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <h1 class="mb-5">User Registration</h1>
+
+        <form @submit.prevent="registerUser(user)">
+            <div class="form-group text-left mb-4">
+                <label for="email" class="font-weight-bold">Email address:</label>
+                <input v-model="user.email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+
+            <div class="form-group text-left mb-4">
+                <label for="password" class="font-weight-bold">Password:</label>
+                <input v-model="user.password" type="password" class="form-control" id="password" placeholder="Enter password">
+            </div>
+
+            <div class="form-group text-left mb-4">
+                <label for="repeatPassword" class="font-weight-bold">Repeat password:</label>
+                <input v-model="user.repeatPassword" type="password" class="form-control" id="repeatPassword" placeholder="Repeat password">
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-block">Registry</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        name: "register",
+        data() {
+            return {
+                user: {
+                    email: '',
+                    password: '',
+                    repeatPassword: ''
+                }
+            }
+        },
+        methods: {
+            ...mapActions(['registerUser'])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
