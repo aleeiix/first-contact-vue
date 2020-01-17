2c8d0d650ad8a043a395bef74716c502cb5228b2
Aleix <aleixriba33@gmail.com>
2020-01-17 19:18:20 +0100

    add calendar in home

---
+++ src/views/Grid.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A distinctio est explicabo labore nisi nostrum quod repellat repellendus veritatis, vero! Accusamus aut autem commodi eligendi iste maxime natus nemo numquam.</p>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eum illo iure laboriosam modi quis tenetur? Animi blanditiis corporis dolore expedita illo, nam odio perspiciatis quod ratione similique suscipit unde.</p>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur, consequuntur culpa deleniti eaque eius maxime modi nemo nisi nostrum odio officiis omnis quaerat quam reprehenderit sequi voluptate voluptatibus voluptatum!</p>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3"xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cumque dicta eaque eum itaque mollitia natus rem repellendus voluptatem voluptatibus. Aspernatur explicabo harum in molestias placeat sint veritatis voluptates voluptatum!</p>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3"xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, alias atque, consectetur culpa deleniti dolorem ea enim exercitationem, facere facilis odit perspiciatis praesentium provident repellendus rerum sapiente similique ullam voluptate!</p>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3"xl="2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut culpa cum ea earum et, facere magnam magni numquam, odio officiis reiciendis soluta voluptas? At beatae commodi ex explicabo odio?</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+
+        <v-card>
+          <v-date-picker
+                  v-model="datepicker"
+                  full-width
+                  locale="es"
+                  :min="min"
+                  :max="max"
+          ></v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">650 - {{datepicker}}</v-card-text>
+        </v-card>
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      min: '1999',
+      max: new Date().toISOString().substr(0, 10),
+      datepicker: null
+    }
   }
 }
 </script>
