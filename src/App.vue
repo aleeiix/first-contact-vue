d35ee3cc752e263f8128d8a798b23c04f41c869f
Aleix <aleixriba33@gmail.com>
2020-01-18 15:11:45 +0100

    login with google and facebook

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -15,46 +11,28 @@
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="mt-1">Vue</span>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn text>
+        <span class="mr-2">Sign Out</span>
         <v-icon>fas fa-external-link-alt</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: "App",
+  data: () => ({})
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-row justify="center" class="mt-5">
+    <v-col cols="12" sm="8" md="6" xl="4">
+      <v-card>
+        <v-card-text
+          class="display-1 text-uppercase white--text text-center"
+          :class="modeRegistry ? 'success' : 'accent'"
+        >
+          <span v-if="!modeRegistry">Log In</span>
+          <span v-if="modeRegistry">Registry</span>
+        </v-card-text>
+
+        <v-card-text>
+          <v-btn @click="loginGoogle" block color="error" class="mb-2">
+            <v-icon left dark>fab fa-google</v-icon>Google
+          </v-btn>
+          <v-btn @click="loginFacebook" block color="info">
+            <v-icon left dark>fab fa-facebook-f</v-icon>Facebook
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text>
+          <v-btn
+            v-if="!modeRegistry"
+            @click="modeRegistry = true"
+            block
+          >You do not have an account? Sign up here</v-btn>
+          <v-btn
+            v-if="modeRegistry"
+            @click="modeRegistry = false"
+            block
+          >Do you already have an account? Login here</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { firebase, auth, firestore } from "@/config/firebase";
+
+export default {
+  data() {
+    return {
+      modeRegistry: false
+    };
+  },
+  methods: {
+    loginGoogle() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      this.login(provider);
+    },
+    loginFacebook() {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      this.login(provider);
+    },
+    async login(provider) {
+      firebase.auth().languageCode = "es";
+
+      try {
+        // Login
+        const result = await firebase.auth().signInWithPopup(provider);
+        const user = result.user;
+        console.log(user);
+
+        // Build userDB
+        const userDB = Object.assign({}, {
+          name: user.displayName,
+          email: user.email,
+          uid: user.uid,
+          photo: user.photoURL
+        });
+
+        // Insert in DB
+        await firestore.collection('users').doc(userDB.uid).set(userDB);
+        console.log('User in DB');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
+</script>
